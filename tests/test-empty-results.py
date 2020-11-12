1a575129299985471c69cef083224f939c68052f
Jeremy Singer-Vine <jsvine@gmail.com>
2020-11-12 09:34:16 -0500

    Update test (endpoint was no longer result-less)

--- tests/test-empty-results.py
+++ tests/test-empty-results.py
@@ -9,7 +9,8 @@ URL = "https://indianexpress.com/section/lifestyle/health/feed/"
 
 class Test(unittest.TestCase):
     def test_empty_result(self):
-        timestamps = waybackpack.search(URL, from_date = "2020")
+        timestamps = waybackpack.search(URL, from_date = "2080")
+
         assert(len(timestamps) == 0)
 
         pack = waybackpack.Pack(
